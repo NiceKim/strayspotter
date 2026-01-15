4c7dd124c6185df1ebf817b58bf2356e902f76ab
김조운 <jowoons_macbook@192.168.1.10>
2026-01-15 22:50:59 +0800

    feat: change configuration to rds from docker db container

--- backend/src/db.js
+++ backend/src/db.js
@@ -136,7 +136,7 @@ async function getValidToken(connection) {
 // Create a single connection pool for the app
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: 'root',
+  user: 'admin',
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
